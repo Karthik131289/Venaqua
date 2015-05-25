8461d9c827ceba3a0664ee3317dba94d6e4e5b38
Karthik <karthik131289@gmail.com>
2015-05-25 21:47:01 +0530

    sample bill

--- src/com/denvik/venaqua/mysql/dbConfig.java
+++ src/com/denvik/venaqua/mysql/dbConfig.java
@@ -1,5 +1,6 @@
 package com.denvik.venaqua.mysql;
 
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,49 +9,50 @@ import java.util.Map;
 
 import com.denvik.venaqua.property.propertyReader;
 
-public class dbConfig extends propertyReader 
-{
+/**
+ * Created by DV21 on 06-03-2015.
+ */
+public class dbConfig extends propertyReader {
     private static final InputStream INPUT_STREAM = dbConfig.class.getResourceAsStream( "dbConfig.properties" );
 
-	public static final String _DRIVER_CLASS = "driverClass";
-	public static final String _DB_URL = "url";
-	public static final String _DB_USERNAME = "userName";
-	public static final String _DB_PASSWORD = "password";
+    public static final String _DRIVERCLASS = "driverClass";
+    public static final String _DB_URL = "url";
+    public static final String _DB_USERNAME = "userName";
+    public static final String _DB_PASSWORD = "password";
 
-	private Map< String , String > defaultProps = new HashMap<String, String>();
+    private Map< String , String > defaultProps = new HashMap<String, String>();
 
     public dbConfig() throws FileNotFoundException , IOException {
         this.initDefaultProperties( this.defaultProps );
         this.loadProperties( INPUT_STREAM , true );
     }
-    
-	@Override
-	public void initDefaultProperties(Map<String, String> defaultVal) {
-	    defaultVal.put( _DRIVER_CLASS , "com.mysql.jdbc.Driver" );
-	    defaultVal.put( _DB_URL , "jdbc:mysql://localhost/venaqua" );
-	    defaultVal.put( _DB_USERNAME , "denvik" );
-	    defaultVal.put( _DB_PASSWORD , "denvik" );
-	    super.setDefaultProps( defaultVal );
-	}
-	
-	@Override
-	public void loadProperties( InputStream inputStream , boolean fromTxtFile) throws FileNotFoundException, IOException {
-	    super.loadProps( inputStream , fromTxtFile );
-	}
-	
-	@Override
-	public String getPropertyValue(String propName) {
-	    return super.getValue( propName );
-	}
-	
-	@Override
-	public void setPropertyValue(String key, String val) {
-	    super.setProperty( key , val );
-	}
-	
-	@Override
-	public String getPropertyValueOrDefault(String propName, String defaultValue) {
-	    return super.getValueOrDefault( propName , defaultValue );
-	}
 
+    @Override
+    public void initDefaultProperties(Map<String, String> defaultVal) {
+        defaultVal.put(_DRIVERCLASS, "com.mysql.jdbc.Driver" );
+        defaultVal.put( _DB_URL , "jdbc:mysql://localhost/venaqua" );
+        defaultVal.put( _DB_USERNAME , "denvik" );
+        defaultVal.put( _DB_PASSWORD , "denvik" );
+        super.setDefaultProps( defaultVal );
+    }
+
+    @Override
+    public void loadProperties( InputStream inputStream , boolean fromTxtFile) throws FileNotFoundException, IOException {
+        super.loadProps( inputStream , fromTxtFile );
+    }
+
+    @Override
+    public String getPropertyValue(String propName) {
+        return super.getValue( propName );
+    }
+
+    @Override
+    public void setPropertyValue(String key, String val) {
+        super.setProperty( key , val );
+    }
+
+    @Override
+    public String getPropertyValueOrDefault(String propName, String defaultValue) {
+        return super.getValueOrDefault( propName , defaultValue );
+    }
 }
--- src/com/denvik/venaqua/mysql/dbUtills.java
+++ src/com/denvik/venaqua/mysql/dbUtills.java
@@ -1,50 +1,73 @@
 package com.denvik.venaqua.mysql;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class dbUtills 
-{
-	private static Connection connection;
-	private static dbConfig config;
-	
-	public static void initDBConnection() 
-	{
-		try 
-		{
-			config = new dbConfig();
-			Class.forName( config.getPropertyValue( config._DRIVER_CLASS ) );
-			connection = DriverManager.getConnection( config.getPropertyValue( config._DB_URL ) , config.getPropertyValue( config._DB_USERNAME ) , config.getPropertyValue( config._DB_PASSWORD ) );
-		} 
-		catch (FileNotFoundException e) 
-		{
-			e.printStackTrace();
-		} 
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		} 
-		catch (ClassNotFoundException e) 
-		{
-			e.printStackTrace();
-		} 
-		catch (SQLException e) 
-		{
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public static Connection getConnection()
-	{
-		return connection;
-	}
+/**
+ * Created by DV21 on 06-03-2015.
+ */
+public class dbUtills {
 
-	public static dbConfig getConfig()
-	{
-		return config;
-	}
+    private static dbConfig config;
+    private static Connection connection;
+
+    public static void initConfig() throws IOException {
+        config = new dbConfig();
+    }
+
+    public static dbConfig getConfig() {
+        return config;
+    }
+
+    public static void initDB() throws ClassNotFoundException, SQLException {
+        Class.forName( config.getPropertyValue( config._DRIVERCLASS ) );
+        connection = DriverManager.getConnection( config.getPropertyValue( config._DB_URL ) , config.getPropertyValue( config._DB_USERNAME ) ,
+                            config.getPropertyValue( config._DB_PASSWORD ) );
+    }
+    public static void closeConnection() throws SQLException {
+        connection.close();
+    }
+    public static Connection getConnection() {
+        return connection;
+    }
+    public static void deleteRecord( String tableName , String fieldName , String conditionString ) throws SQLException {
+        Statement statement = null;
+        String queryString  = null;
+
+        statement = getConnection().createStatement();
+        queryString = "DELETE FROM " + tableName + " WHERE " + fieldName + " " + conditionString + ";";
+        statement.execute( queryString );
+        statement.close();
+    }
+    public static void insertRecord( String tableName , String[] fieldNames , Object[] fieldValues ) throws SQLException {
+        Statement statement = null;
+        String queryString  = null;
+
+        statement = getConnection().createStatement();
+
+        queryString = "INSERT INTO " + tableName + " (";
+        for( int i=0; i<fieldNames.length; i++ ) {
+            queryString = queryString + fieldNames[i];
+            if( i!=(fieldNames.length-1) )
+                queryString = queryString + ",";
+            else
+                queryString = queryString + ") VALUES (";
+        }
+        for (int i=0;i<fieldValues.length; i++ ) {
+            if( fieldValues[i] instanceof String )
+                queryString = queryString + "'" + fieldValues[i] + "'";
+            else
+                queryString = queryString + fieldValues[i];
+            if( i!=(fieldValues.length-1) )
+                queryString = queryString + ",";
+            else
+                queryString = queryString + ");";
+        }
+
+        statement.execute( queryString );
+        statement.close();
+    }
 }
